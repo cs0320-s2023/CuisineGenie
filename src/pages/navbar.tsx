da8cd303915297ae9f47d10e8de435a2d47a9c32
kaylamukai <kayla_mukai@brown.edu>
2023-04-25 13:47:10 -0400

    navbar margin issue

--- src/pages/navbar.tsx
+++ src/pages/navbar.tsx
@@ -1,31 +1,48 @@
 import { BrowserRouter, Route, Link } from "react-router-dom";
-import './navbar.css';
+import "./navbar.css";
 
 export default function Navbar() {
   return (
     <nav className=" navbar navbar-expand-lg navbar-light bg-light">
-      <div className = "container-fluid">
-        <a className="navbar-brand" href="#">🧞 CuisineGenie</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          🧞 CuisineGenie
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div className ="collapse navbar-collapse ms-5" id="navbarSupportedContent" >
-          <ul className ="navbar-nav ms-auto">
-              <li className="navbar-item navbar-quiz">
-              <Link className="navbar-item link ms-5" to="/quiz">quiz</Link>
-              </li>
-              <li className="navbar-item navbar-results">
-                <Link className="navbar-item link ms-5" to="/results">results</Link>
-              </li>
-              <li className="navbar-item navbar-profile">
-                <Link className="navbar-item link ms-5" to="/profile">profile</Link>
-              </li>
-            </ul>
+        <div
+          className="collapse navbar-collapse ms-5"
+          id="navbarSupportedContent"
+        >
+          <ul className="navbar-nav ms-auto">
+            <li className="navbar-item navbar-quiz">
+              <Link className="navbar-item link ms-5" to="/quiz">
+                quiz
+              </Link>
+            </li>
+            <li className="navbar-item navbar-results">
+              <Link className="navbar-item link ms-5" to="/results">
+                results
+              </Link>
+            </li>
+            <li className="navbar-item navbar-profile">
+              <Link className="navbar-item link ms-5" to="/profile">
+                profile
+              </Link>
+            </li>
+          </ul>
         </div>
       </div>
     </nav>
   );
 }
-
-
--- src/pages/results.tsx
+++ src/pages/results.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import Navbar from './navbar';
+import React from "react";
+import Navbar from "./navbar";
+import "./navbar.css";
 
 export default function Results() {
-    return(
-        <div>
-        <Navbar />
-        <div className="bottom-padding"></div>
-      </div>
-
-    );
+  return (
+    <div style={{ margin: "0", padding: "0" }}>
+      <div className="nav-color"></div>
+      <Navbar />
+      <div className="bottom-padding"></div>
+    </div>
+  );
 }
