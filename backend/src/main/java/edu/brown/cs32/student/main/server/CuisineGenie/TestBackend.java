e120585c01b393364b731eb2927b099ad2cca25a
kmao5 <katherine@lightyearstrategies.com>
2023-05-06 14:51:05 -0400

    backend tests

--- backend/src/main/java/edu/brown/cs32/student/main/server/CuisineGenie/TestBackend.java
+++ backend/src/main/java/edu/brown/cs32/student/main/server/CuisineGenie/TestBackend.java
@@ -24,7 +24,7 @@ import org.junit.jupiter.api.Test;
 import okio.Buffer;
 import spark.Spark;
 
-public class TestBackend() {
+public class TestBackend {
 
     @BeforeAll
     public static void setup_before_everything() {
@@ -35,7 +35,6 @@ public class TestBackend() {
     @BeforeEach
     public void setup() {
         // Server server = new Server();
-        List<List<String>> csv = new ArrayList<>();
         Spark.get("generaterecipes", new Generator());
         Spark.init();
         Spark.awaitInitialization();
@@ -67,14 +66,15 @@ public class TestBackend() {
     @Test
     public void AllSameCategory() throws IOException {
         // beef
-        HttpURLConnection clientConnection = tryRequest("generaterecipe?1=52874&2=52878&3=53071&4=52997&5=52904");
+        HttpURLConnection clientConnection = tryRequest("generaterecipes?1=52874&2=52878&3=53071&4=52997&5=52904");
         assertEquals(200, clientConnection.getResponseCode());
 
         Moshi moshi = new Moshi.Builder().build();
         JsonAdapter<Map<String, Object>> adapter = moshi.adapter(
                 Types.newParameterizedType(Map.class, String.class, Object.class));
         Map response = adapter.fromJson(new Buffer().readFrom(clientConnection.getInputStream()));
-        assertEquals("[]", response.get("ids").toString()); // fill in
+        assertEquals("[52874, 52878, 53071, 52997, 52904, 53070, 52873, 53068, 52824, 52803, 53069, 52876, 52881, 52935]", response.get("ids").toString());
+  
 
         // clientConnection = tryRequest("searchcsv?target=Sol&hasHeader=true&colID=ProperName");
         // assertEquals(200, clientConnection.getResponseCode());
@@ -89,47 +89,58 @@ public class TestBackend() {
     }
 
     @Test
-    public void AllDifferentCategories() throws IOException {
+    public void AllDifferentCategories() throws IOException { // this one is weird bc it's random.....what to do????
         // beef, chicken, vegetarian, dessert, breakfast
-        HttpURLConnection clientConnection = tryRequest("generaterecipe?1=52874&2=53050&3=52807&4=53049&5=52965");
+        HttpURLConnection clientConnection = tryRequest("generaterecipes?1=52874&2=53050&3=52807&4=53049&5=52965");
         assertEquals(200, clientConnection.getResponseCode());
 
         Moshi moshi = new Moshi.Builder().build();
         JsonAdapter<Map<String, Object>> adapter = moshi.adapter(
                 Types.newParameterizedType(Map.class, String.class, Object.class));
         Map response = adapter.fromJson(new Buffer().readFrom(clientConnection.getInputStream()));
-        assertEquals("[]", response.get("ids").toString()); // fill in
+        assertEquals("[52874, 52878, 53071, 52997, 52904, 53070, 52873, 53068, 53053, 52824, 52803, 53069, 52876, 52927, 52930, 53052, 52881, 52935]", response.get("ids").toString()); // fill in
     }
 
     @Test
     public void OneCommonCategory() throws IOException {
         // chicken, chicken, vegetarian, dessert, breakfast
-        HttpURLConnection clientConnection = tryRequest("generaterecipe?1=52765&2=53050&3=52807&4=53049&5=52965");
+        HttpURLConnection clientConnection = tryRequest("generaterecipes?1=52765&2=53050&3=52807&4=53049&5=52965");
         assertEquals(200, clientConnection.getResponseCode());
 
         Moshi moshi = new Moshi.Builder().build();
         JsonAdapter<Map<String, Object>> adapter = moshi.adapter(
                 Types.newParameterizedType(Map.class, String.class, Object.class));
         Map response = adapter.fromJson(new Buffer().readFrom(clientConnection.getInputStream()));
-        assertEquals("[]", response.get("ids").toString()); // fill in
+        assertEquals("[53050, 52846, 52934, 52765, 52875, 52795, 52920, 52879, 52832, 52830, 52851, 52933, 52806]", response.get("ids").toString()); 
     }
 
     @Test
-    public void TieCategories() throws IOException {
+    public void TieCategories() throws IOException { // it will take the first most common category
         // chicken, chicken, vegetarian, vegetarian, breakfast
-        HttpURLConnection clientConnection = tryRequest("generaterecipe?1=52765&2=53050&3=52807&4=52771&5=52965");
+        HttpURLConnection clientConnection = tryRequest("generaterecipes?1=52765&2=53050&3=52807&4=52771&5=52965");
         assertEquals(200, clientConnection.getResponseCode());
 
         Moshi moshi = new Moshi.Builder().build();
         JsonAdapter<Map<String, Object>> adapter = moshi.adapter(
                 Types.newParameterizedType(Map.class, String.class, Object.class));
         Map response = adapter.fromJson(new Buffer().readFrom(clientConnection.getInputStream()));
-        assertEquals("[]", response.get("ids").toString()); // fill in
+        assertEquals("[53050, 52846, 52796, 52934, 52765, 52875, 52795, 52920, 52879, 52832, 52830, 52851, 52933, 52806]", response.get("ids").toString()); 
     }
 
     @Test
-    public void AllSameAreas() {
+    public void AllSameAreas() throws IOException {
+        // american
+        // dessert, pork, beef, beef, chicken
 
+        // why is most common chicken??? should be beef
+        HttpURLConnection clientConnection = tryRequest("generaterecipes?1=52855&2=52995&3=52812&4=53013&5=53016");
+        assertEquals(200, clientConnection.getResponseCode());
+
+        Moshi moshi = new Moshi.Builder().build();
+        JsonAdapter<Map<String, Object>> adapter = moshi.adapter(
+                Types.newParameterizedType(Map.class, String.class, Object.class));
+        Map response = adapter.fromJson(new Buffer().readFrom(clientConnection.getInputStream()));
+        assertEquals("", response.get("ids").toString()); 
     }
 
     @Test
@@ -139,26 +150,26 @@ public class TestBackend() {
 
     // Failure Responses
     @Test
-    public void WrongNumberParams() {
+    public void WrongNumberParams() throws IOException {
         // 6
-        HttpURLConnection clientConnection = tryRequest("generaterecipe?1=52765&2=53050&3=52807&4=52771&5=52965&6=52871");
+        HttpURLConnection clientConnection = tryRequest("generaterecipes?1=52765&2=53050&3=52807&4=52771&5=52965&6=52871");
         assertEquals(200, clientConnection.getResponseCode());
 
         Moshi moshi = new Moshi.Builder().build();
         JsonAdapter<Map<String, Object>> adapter = moshi.adapter(
                 Types.newParameterizedType(Map.class, String.class, Object.class));
         Map response = adapter.fromJson(new Buffer().readFrom(clientConnection.getInputStream()));
-        assertEquals("[]", response.get("errorMessage").toString()); // fill in
+        assertEquals("Please input 5 recipes IDs.", response.get("errorMessage").toString()); 
 
         // 4
-        HttpURLConnection clientConnection = tryRequest("generaterecipe?1=52765&2=53050&3=52807&4=52771");
+        clientConnection = tryRequest("generaterecipes?1=52765&2=53050&3=52807&4=52771");
         assertEquals(200, clientConnection.getResponseCode());
 
-        Moshi moshi = new Moshi.Builder().build();
-        JsonAdapter<Map<String, Object>> adapter = moshi.adapter(
+        Moshi moshi2 = new Moshi.Builder().build();
+        JsonAdapter<Map<String, Object>> adapter2 = moshi2.adapter(
                 Types.newParameterizedType(Map.class, String.class, Object.class));
-        Map response = adapter.fromJson(new Buffer().readFrom(clientConnection.getInputStream()));
-        assertEquals("[]", response.get("errorMessage").toString()); // fill in
+        Map response2 = adapter2.fromJson(new Buffer().readFrom(clientConnection.getInputStream()));
+        assertEquals("Please input 5 recipes IDs.", response2.get("errorMessage").toString()); 
     }
 }
 
